c389593d6509733532cf37c1cfbcc6ae0b0fb962
abhinand v k <abhinandazokdio@gmail.com>
2022-10-27 23:15:01 +0530

    updated

---
+++ model/data.js
@@ -0,0 +1,44 @@
+const songs = [
+  {
+    id: 1,
+    title: '19th Floor',
+    artist: 'Bobby Richards',
+    artwork:
+      'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/img1.jpg?alt=media&token=ad99f9e5-6606-4f88-a1c3-dd0d4dda57ab',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/19th%20Floor%20-%20Bobby%20Richards.mp3?alt=media&token=7197569c-ab53-42fe-a2d3-286592206c00',
+  },
+  {
+    id: 2,
+    title: 'Awful',
+    artist: 'josh pan',
+    artwork:
+      'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/img2.jpg?alt=media&token=e9055b65-380b-4149-a2c4-fca97476d92a',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/Awful%20-%20josh%20pan.mp3?alt=media&token=6f63bd2f-32ed-4d0a-bfb1-f69a1ff20c85',
+  },
+  {
+    id: 3,
+    title: 'Something is Going On',
+    artist: 'Godmode',
+    artwork:
+      'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/img3.jpg?alt=media&token=b16d426d-36b1-42bb-aa87-ab784eae9eba',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/Something%20is%20Going%20On%20-%20Godmode.mp3?alt=media&token=ab9ef532-ab33-4229-a521-65f033a7de88',
+  },
+  {
+    id: 4,
+    title: 'Book The Rental Wit It',
+    artist: 'RAGE',
+    artwork:
+      'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/img4.jpg?alt=media&token=8b58381b-9812-4753-97ab-16fe8d51ba85',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/Book%20The%20Rental%20Wit%20It%20-%20RAGE.mp3?alt=media&token=94e541db-d1f8-4af5-ba14-55fdc470da7b',
+  },
+  {
+    id: 5,
+    title: 'Crimson Fly',
+    artist: 'Huma-Huma',
+    artwork:
+      'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/img5.jpg?alt=media&token=3c07afe7-11fa-4fbf-a4c1-41c33fa889a2',
+    url: 'https://firebasestorage.googleapis.com/v0/b/fir-dc48c.appspot.com/o/Crimson%20Fly%20-%20Huma-Huma.mp3?alt=media&token=54f13761-9b94-4122-8435-b43d7c6ff777',
+  },
+];
+
+export default songs;
